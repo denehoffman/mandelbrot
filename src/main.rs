d8695e2ddf2b546105871f464c07a01db34b1238
Nathaniel D. Hoffman <dene@cmu.edu>
2024-03-23 15:54:04 +0530

    add an option to invert colors and an option to use higher-precision decimals at the cost of speed

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,15 @@
 use clap::Parser;
 use colorgrad::Gradient;
-use num_complex::Complex;
+use fpdec::Decimal;
+use num_complex::{Complex, ComplexFloat};
 use pix_engine::prelude::*;
 use rayon::prelude::*;
-use rust_decimal::prelude::*;
 
-type Dec = f64;
-type ComplexDec = Complex<Dec>;
-
-fn evaluate(x: Dec, y: Dec, max_iters: usize) -> f64 {
-    let mut z = ComplexDec::default();
-    let c = ComplexDec::new(x, y);
+fn evaluate<T: Num + TryFrom<f64>>(x: T, y: T, max_iters: usize) -> f64 {
+    let mut z = Complex::<T>::default();
+    let c = Complex::<T>::new(x, y);
     for i in 0..max_iters {
-        if z.norm_sqr() >= Dec::from_f64(4.0).unwrap() {
+        if z.norm_sqr() >= from_f64(4.0) {
             return (i - 1) as f64;
         }
         z = z * z + c;
@@ -20,32 +17,59 @@ fn evaluate(x: Dec, y: Dec, max_iters: usize) -> f64 {
     max_iters as f64
 }
 
-struct Mandelbrot {
-    x_min: Vec<Dec>,
-    x_max: Vec<Dec>,
-    y_min: Vec<Dec>,
-    y_max: Vec<Dec>,
+struct Mandelbrot<T> {
+    x_min: Vec<T>,
+    x_max: Vec<T>,
+    y_min: Vec<T>,
+    y_max: Vec<T>,
     set_storage: Vec<Vec<f64>>,
     max_iters: usize,
     colors: Gradient,
     show_box: bool,
+    inverted: bool,
 }
 
 fn remap<T: Num>(val: T, a: T, b: T, c: T, d: T) -> T {
     (val - a) * (d - c) / (b - a) + c
 }
 
-impl Mandelbrot {
-    fn new(height: usize, width: usize, colors: Gradient) -> Self {
+fn from_f64<T: Num + TryFrom<f64>>(val: f64) -> T {
+    match T::try_from(val) {
+        Ok(v) => v,
+        Err(_) => panic!("Conversion error!"),
+    }
+}
+fn from_i32<T: Num + TryFrom<i32>>(val: i32) -> T {
+    match T::try_from(val) {
+        Ok(v) => v,
+        Err(_) => panic!("Conversion error!"),
+    }
+}
+fn from_u32<T: Num + TryFrom<u32>>(val: u32) -> T {
+    match T::try_from(val) {
+        Ok(v) => v,
+        Err(_) => panic!("Conversion error!"),
+    }
+}
+
+impl<T: Num + TryFrom<f64> + TryFrom<u32> + Sync> Mandelbrot<T> {
+    fn new(
+        height: usize,
+        width: usize,
+        colors: Gradient,
+        max_iters: usize,
+        inverted: bool,
+    ) -> Self {
         Self {
-            x_min: vec![Dec::from_f64(-2.0).unwrap()],
-            x_max: vec![Dec::from_f64(0.5).unwrap()],
-            y_min: vec![Dec::from_f64(-2.0).unwrap()],
-            y_max: vec![Dec::from_f64(2.0).unwrap()],
+            x_min: vec![from_f64(-2.0)],
+            x_max: vec![from_f64(0.5)],
+            y_min: vec![from_f64(-2.0)],
+            y_max: vec![from_f64(2.0)],
             set_storage: vec![vec![0.0; height]; width],
-            max_iters: 2000,
+            max_iters,
             colors,
             show_box: false,
+            inverted,
         }
     }
     fn update_set(&mut self, s: &mut PixState) -> PixResult<()> {
@@ -54,19 +78,19 @@ impl Mandelbrot {
         self.set_storage = (0..width)
             .into_par_iter()
             .map(|u| {
-                let x = remap(
-                    Dec::from(u),
-                    Dec::from_f64(0.0).unwrap(),
-                    Dec::from(width),
+                let x: T = remap(
+                    from_u32(u),
+                    from_f64(0.0),
+                    from_u32(width),
                     *self.x_min.last().unwrap(),
                     *self.x_max.last().unwrap(),
                 );
                 (0..height)
                     .map(|v| {
-                        let y = remap(
-                            Dec::from(v),
-                            Dec::from_f64(0.0).unwrap(),
-                            Dec::from(height),
+                        let y: T = remap(
+                            from_u32(v),
+                            from_f64(0.0),
+                            from_u32(height),
                             *self.y_min.last().unwrap(),
                             *self.y_max.last().unwrap(),
                         );
@@ -80,7 +104,10 @@ impl Mandelbrot {
     fn draw_set(&self, s: &mut PixState) -> PixResult<()> {
         for u in 0..s.width()? {
             for v in 0..s.height()? {
-                let iters = self.set_storage[u as usize][v as usize];
+                let mut iters = self.set_storage[u as usize][v as usize];
+                if !self.inverted {
+                    iters = self.max_iters as f64 - iters;
+                }
                 s.stroke(Color::from_slice(
                     ColorMode::Rgb,
                     self.colors
@@ -97,7 +124,10 @@ impl Mandelbrot {
     }
 }
 
-impl PixEngine for Mandelbrot {
+impl<T> PixEngine for Mandelbrot<T>
+where
+    T: Num + TryFrom<f64> + TryFrom<i32> + TryFrom<u32> + Sync,
+{
     fn on_start(&mut self, s: &mut PixState) -> PixResult<()> {
         s.rect_mode(RectMode::Center);
         s.fill(None);
@@ -121,30 +151,30 @@ impl PixEngine for Mandelbrot {
     ) -> PixResult<bool> {
         if let Mouse::Left = btn {
             let x0 = remap(
-                Dec::from(pos.x() - 50),
-                Dec::from_f64(0.0).unwrap(),
-                Dec::from((s.width())?),
+                from_i32(pos.x() - 50),
+                from_f64(0.0),
+                from_u32(s.width()?),
                 *self.x_min.last().unwrap(),
                 *self.x_max.last().unwrap(),
             );
             let y0 = remap(
-                Dec::from(pos.y() - 50),
-                Dec::from_f64(0.0).unwrap(),
-                Dec::from((s.width())?),
+                from_i32(pos.y() - 50),
+                from_f64(0.0),
+                from_u32(s.width()?),
                 *self.y_min.last().unwrap(),
                 *self.y_max.last().unwrap(),
             );
             let x1 = remap(
-                Dec::from(pos.x() + 50),
-                Dec::from_f64(0.0).unwrap(),
-                Dec::from((s.width())?),
+                from_i32(pos.x() + 50),
+                from_f64(0.0),
+                from_u32(s.width()?),
                 *self.x_min.last().unwrap(),
                 *self.x_max.last().unwrap(),
             );
             let y1 = remap(
-                Dec::from(pos.y() + 50),
-                Dec::from_f64(0.0).unwrap(),
-                Dec::from((s.width())?),
+                from_i32(pos.y() + 50),
+                from_f64(0.0),
+                from_u32(s.width()?),
                 *self.y_min.last().unwrap(),
                 *self.y_max.last().unwrap(),
             );
@@ -242,6 +272,16 @@ struct Args {
 
     #[arg(short, long, default_value_t = 600)]
     width: usize,
+
+    #[arg(short, long, default_value_t = 500)]
+    max_iters: usize,
+
+    #[arg(long)]
+    inverse: bool,
+
+    #[arg(long)]
+    // Slower, but uses fixed decimal numbers for higher zoom accuracy
+    precise: bool,
 }
 
 fn main() -> PixResult<()> {
@@ -250,6 +290,23 @@ fn main() -> PixResult<()> {
         .dimensions(args.width as u32, args.height as u32)
         .title("Mandelbrot")
         .build()?;
-    let mut app = Mandelbrot::new(args.width, args.height, gradient_from_string(&args.color));
-    engine.run(&mut app)
+    if args.precise {
+        let mut app = Mandelbrot::<Decimal>::new(
+            args.width,
+            args.height,
+            gradient_from_string(&args.color),
+            args.max_iters,
+            args.inverse,
+        );
+        engine.run(&mut app)
+    } else {
+        let mut app = Mandelbrot::<f64>::new(
+            args.width,
+            args.height,
+            gradient_from_string(&args.color),
+            args.max_iters,
+            args.inverse,
+        );
+        engine.run(&mut app)
+    }
 }
